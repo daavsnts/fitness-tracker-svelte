861f1be350bf3c5d46248899a22e97bf64aba961
Victor André Gris Costa <victorandr98@gmail.com>
2023-04-28 02:05:09 -0300

    structure (refactor): move components and stories to desired folder structure and adjust aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      $components: path.resolve("./src/components"),
+      $assets: path.resolve("./src/assets"),
+      $pages: path.resolve("./src/pages"),
+      $helpers: path.resolve("./src/helpers"),
+    }
+  }
 })
