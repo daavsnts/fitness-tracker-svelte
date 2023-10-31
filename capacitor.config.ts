5a1bf77749dd622c55b24cb845edf9705a2920aa
daavsnts <daavsantosl@gmail.com>
2023-10-31 02:37:38 -0300

    SQLite (feat): add capacitor sqlite plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,23 @@ const config: CapacitorConfig = {
   appName: "capacitive-svelte-template",
   webDir: "dist",
   bundledWebRuntime: false,
+  plugins: {
+    CapacitorSQLite: {
+      iosDatabaseLocation: "Library/CapacitorDatabase",
+      iosIsEncryption: true,
+      iosKeychainPrefix: "angular-sqlite-app-starter",
+      iosBiometric: {
+        biometricAuth: false,
+        biometricTitle: "Biometric login for capacitor sqlite",
+      },
+      androidIsEncryption: true,
+      androidBiometric: {
+        biometricAuth: false,
+        biometricTitle: "Biometric login for capacitor sqlite",
+        biometricSubTitle: "Log in using your biometric",
+      },
+    },
+  },
 };
 
 export default config;
