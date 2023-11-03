ed6c75945e22c635670a1c627f0c74bccd6f8a73
daavsnts <daavsantosl@gmail.com>
2023-11-03 04:56:27 -0300

    AppContainer (feat): add ExerciseRepository

--- src/di/AppContainer.ts
+++ src/di/AppContainer.ts
@@ -1,20 +1,26 @@
 import { FirestoreDao } from "../data/network/FirestoreDao";
 import { FirebaseService } from "../data/network/FirebaseService";
 import { WaterRepository } from "../data/repositories/WaterRepository";
+import { ExerciseRepository } from "../data/repositories/ExerciseRepository";
 
 class AppContainer {
   private _firebaseService: FirebaseService;
   private _firebaseDao: FirestoreDao;
   private _waterRepository: WaterRepository;
+  private _exerciseRepository: ExerciseRepository;
 
   constructor() {
     this._firebaseService = new FirebaseService();
     this._firebaseDao = new FirestoreDao(this._firebaseService.db);
     this._waterRepository = new WaterRepository(this._firebaseDao);
+    this._exerciseRepository = new ExerciseRepository(this._firebaseDao);
   }
 
   get waterRepository() {
     return this._waterRepository;
+  }
+  get exerciseRepository() {
+    return this._exerciseRepository;
   }
 }
 
