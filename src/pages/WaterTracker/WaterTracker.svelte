494c26bd59a3e5adc2d90a689e185850608216bd
daavsnts <daavsantosl@gmail.com>
2023-11-03 03:45:20 -0300

    WaterTracker page (feat): pré-built page to test App architecture

--- src/pages/WaterTracker/WaterTracker.svelte
+++ src/pages/WaterTracker/WaterTracker.svelte
@@ -3,9 +3,23 @@
 
   let todayTotalWaterIntake = waterTrackerStore.todayTotalWaterIntake;
   let currentWaterGoal = waterTrackerStore.currentWaterGoal;
+
+  let addWaterIntakeValue = 0;
+  let updateTodayWaterGoalValue = 0;
 </script>
 
 <h1>{$todayTotalWaterIntake}/{$currentWaterGoal.quantity}</h1>
+
+<input bind:value={addWaterIntakeValue} />
+<button on:click={() => waterTrackerStore.addWaterIntake(addWaterIntakeValue)}
+  >Add</button
+>
+<input bind:value={updateTodayWaterGoalValue} />
+<button
+  on:click={() =>
+    waterTrackerStore.updateTodayWaterGoal(updateTodayWaterGoalValue)}
+  >Set</button
+>
 
 <style lang="scss">
 </style>
