6c8e68e481eda5619b807c8a31c4fdb13ed2930d
daavsnts <daavsantosl@gmail.com>
2023-11-09 12:34:10 -0300

    SQLiteExerciseDao (refactor): change Exercise's type/table names

--- src/data/local/SQLiteExerciseDao.ts
+++ src/data/local/SQLiteExerciseDao.ts
@@ -1,16 +1,18 @@
-import type { Exercise, ExerciseGoal } from "$types/fitnessTypes";
+import type { ExercisePause, ExercisePausesGoal } from "$types/fitnessTypes";
 import type { SQLiteDBConnection } from "@capacitor-community/sqlite";
 import type { SQLiteDao } from "./SQLiteDao";
 
 export interface SQLiteExerciseDao {
-  getExerciseHistory: () => Promise<Exercise[]>;
-  getTodayExerciseHistory: () => Promise<Exercise[]>;
-  getLatestExerciseGoal: () => Promise<ExerciseGoal>;
-  getTodayExerciseGoal: () => Promise<ExerciseGoal>;
-  getExerciseGoalHistory: () => Promise<ExerciseGoal[]>;
-  getTodayExerciseGoalHistory: () => Promise<ExerciseGoal[]>;
-  addExercise: (exercise: Exercise) => Promise<boolean>;
-  updateTodayExerciseGoal: (exerciseGoal: ExerciseGoal) => Promise<boolean>;
+  getExercisePausesHistory: () => Promise<ExercisePause[]>;
+  getTodayExercisePausesHistory: () => Promise<ExercisePause[]>;
+  getLatestExercisePausesGoal: () => Promise<ExercisePausesGoal>;
+  getTodayExercisePausesGoal: () => Promise<ExercisePausesGoal>;
+  getExercisePausesGoalHistory: () => Promise<ExercisePausesGoal[]>;
+  getTodayExercisePausesGoalHistory: () => Promise<ExercisePausesGoal[]>;
+  addExercisePause: (exercisePause: ExercisePause) => Promise<boolean>;
+  updateTodayExercisePausesGoal: (
+    exercisePausesGoal: ExercisePausesGoal
+  ) => Promise<boolean>;
 }
 
 export async function createSQLiteExerciseDao(
@@ -19,57 +21,64 @@ export async function createSQLiteExerciseDao(
 ): Promise<SQLiteExerciseDao> {
   const dbConnection = await dbConnectionPromise;
   const dao = await daoPromise;
-  const EXERCISE_TABLE = "exercise_log";
-  const EXERCISE_GOAL_TABLE = "exercise_goal_log";
+  const EXERCISE_PAUSES_TABLE = "exercise_pauses_log";
+  const EXERCISE_PAUSES_GOAL_TABLE = "exercise_pauses_goal_log";
 
-  async function getExerciseHistory(): Promise<Exercise[]> {
-    return await dao.getHistory(EXERCISE_TABLE);
+  async function getExercisePausesHistory(): Promise<ExercisePause[]> {
+    return await dao.getHistory(EXERCISE_PAUSES_TABLE);
   }
 
-  async function getTodayExerciseHistory(): Promise<Exercise[]> {
-    return await dao.getTodayHistory(EXERCISE_TABLE);
+  async function getTodayExercisePausesHistory(): Promise<ExercisePause[]> {
+    return await dao.getTodayHistory(EXERCISE_PAUSES_TABLE);
   }
 
-  async function getLatestExerciseGoal(): Promise<ExerciseGoal> {
-    return await dao.getLatest(EXERCISE_GOAL_TABLE);
+  async function getLatestExercisePausesGoal(): Promise<ExercisePausesGoal> {
+    return await dao.getLatest(EXERCISE_PAUSES_GOAL_TABLE);
   }
 
-  async function getTodayExerciseGoal(): Promise<ExerciseGoal> {
-    return await dao.getToday(EXERCISE_GOAL_TABLE);
+  async function getTodayExercisePausesGoal(): Promise<ExercisePausesGoal> {
+    return await dao.getToday(EXERCISE_PAUSES_GOAL_TABLE);
   }
 
-  async function getExerciseGoalHistory(): Promise<ExerciseGoal[]> {
-    return await dao.getHistory(EXERCISE_GOAL_TABLE);
+  async function getExercisePausesGoalHistory(): Promise<ExercisePausesGoal[]> {
+    return await dao.getHistory(EXERCISE_PAUSES_GOAL_TABLE);
   }
 
-  async function getTodayExerciseGoalHistory(): Promise<ExerciseGoal[]> {
-    return await dao.getTodayHistory(EXERCISE_GOAL_TABLE);
+  async function getTodayExercisePausesGoalHistory(): Promise<
+    ExercisePausesGoal[]
+  > {
+    return await dao.getTodayHistory(EXERCISE_PAUSES_GOAL_TABLE);
   }
 
-  async function addExercise(exercise: Exercise): Promise<boolean> {
+  async function addExercisePause(
+    exercisePause: ExercisePause
+  ): Promise<boolean> {
     await dbConnection.open();
-    const addQuantityResponse = dbConnection.run(
-      `INSERT INTO ${EXERCISE_TABLE} (type, timeStamp) VALUES (?, ?);`,
-      [exercise.type, exercise.timeStamp.toISOString()]
+    const addExercisePauseResponse = dbConnection.run(
+      `INSERT INTO ${EXERCISE_PAUSES_TABLE} (type, timeStamp) VALUES (?, ?);`,
+      [exercisePause.type, exercisePause.timeStamp.toISOString()]
     );
     await dbConnection.close();
-    return (await addQuantityResponse).changes.changes == 1;
+    return (await addExercisePauseResponse).changes.changes == 1;
   }
 
-  async function updateTodayExerciseGoal(
-    waterGoal: ExerciseGoal
+  async function updateTodayExercisePausesGoal(
+    exercisePausesGoal: ExercisePausesGoal
   ): Promise<boolean> {
-    return await dao.updateTodayGoal(waterGoal, EXERCISE_GOAL_TABLE);
+    return await dao.updateTodayGoal(
+      exercisePausesGoal,
+      EXERCISE_PAUSES_GOAL_TABLE
+    );
   }
 
   return {
-    getExerciseHistory,
-    getTodayExerciseHistory,
-    getLatestExerciseGoal,
-    getTodayExerciseGoal,
-    getExerciseGoalHistory,
-    getTodayExerciseGoalHistory,
-    addExercise,
-    updateTodayExerciseGoal,
+    getExercisePausesHistory,
+    getTodayExercisePausesHistory,
+    getLatestExercisePausesGoal,
+    getTodayExercisePausesGoal,
+    getExercisePausesGoalHistory,
+    getTodayExercisePausesGoalHistory,
+    addExercisePause,
+    updateTodayExercisePausesGoal,
   };
 }
