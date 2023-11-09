1a952fcac6ca05790a621b750bf144abbc3daf2c
daavsnts <daavsantosl@gmail.com>
2023-11-09 07:57:35 -0300

    fitnessTypes (feat): add TodayInterval class

--- src/types/fitnessTypes.ts
+++ src/types/fitnessTypes.ts
@@ -17,3 +17,13 @@ export type ExerciseGoal = {
   quantity: number;
   timeStamp: Date;
 };
+
+export class TodayInterval {
+  public start: Date;
+  public end: Date;
+
+  constructor() {
+    this.start = new Date(new Date().setHours(0, 0, 0, 0));
+    this.end = new Date(new Date().setHours(23, 59, 59, 999));
+  }
+}
