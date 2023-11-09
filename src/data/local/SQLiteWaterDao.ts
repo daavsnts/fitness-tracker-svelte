f0220cdaa98487d1a34e79ae670db66c10f76614
daavsnts <daavsantosl@gmail.com>
2023-11-09 10:40:08 -0300

    SQLiteWaterDao (feat): add SQLiteWaterDao

---
+++ src/data/local/SQLiteWaterDao.ts
@@ -0,0 +1,64 @@
+import type { WaterGoal, WaterIntake } from "$types/fitnessTypes";
+import type { SQLiteDao } from "./SQLiteDao";
+
+export interface SQLiteWaterDao {
+  getWaterIntakeHistory: () => Promise<WaterIntake[]>;
+  getTodayWaterIntakeHistory: () => Promise<WaterIntake[]>;
+  getLatestWaterGoal: () => Promise<WaterGoal>;
+  getTodayLatestWaterGoal: () => Promise<WaterGoal>;
+  getWaterGoalHistory: () => Promise<WaterGoal[]>;
+  getTodayWaterGoalHistory: () => Promise<WaterGoal[]>;
+  addWaterIntake: (waterIntake: WaterIntake) => Promise<boolean>;
+  updateTodayWaterGoal: (waterGoal: WaterGoal) => Promise<boolean>;
+}
+
+export async function createSQLiteWaterDao(
+  daoPromise: Promise<SQLiteDao>
+): Promise<SQLiteWaterDao> {
+  const dao = await daoPromise;
+  const WATER_INTAKE_TABLE = "water_intake_log";
+  const WATER_GOAL_TABLE = "water_goal_log";
+
+  async function getWaterIntakeHistory(): Promise<WaterIntake[]> {
+    return await dao.getHistory(WATER_INTAKE_TABLE);
+  }
+
+  async function getTodayWaterIntakeHistory(): Promise<WaterIntake[]> {
+    return await dao.getTodayHistory(WATER_INTAKE_TABLE);
+  }
+
+  async function getLatestWaterGoal(): Promise<WaterGoal> {
+    return await dao.getLatest(WATER_GOAL_TABLE);
+  }
+
+  async function getTodayLatestWaterGoal(): Promise<WaterGoal> {
+    return await dao.getTodayLatest(WATER_GOAL_TABLE);
+  }
+
+  async function getWaterGoalHistory(): Promise<WaterGoal[]> {
+    return await dao.getHistory(WATER_GOAL_TABLE);
+  }
+
+  async function getTodayWaterGoalHistory(): Promise<WaterGoal[]> {
+    return await dao.getTodayHistory(WATER_GOAL_TABLE);
+  }
+
+  async function addWaterIntake(waterIntake: WaterIntake): Promise<boolean> {
+    return await dao.addQuantity(waterIntake, WATER_INTAKE_TABLE);
+  }
+
+  async function updateTodayWaterGoal(waterGoal: WaterGoal): Promise<boolean> {
+    return await dao.updateTodayGoal(waterGoal, WATER_GOAL_TABLE);
+  }
+
+  return {
+    getWaterIntakeHistory,
+    getTodayWaterIntakeHistory,
+    getLatestWaterGoal,
+    getTodayLatestWaterGoal,
+    getWaterGoalHistory,
+    getTodayWaterGoalHistory,
+    addWaterIntake,
+    updateTodayWaterGoal,
+  };
+}
