f59e0eddb5d6ad25c5a76e1534f4953b11e52ee8
daavsnts <daavsantosl@gmail.com>
2023-11-09 10:53:27 -0300

    WaterTrackerModal (feat): add WaterTrackerModal component

---
+++ src/components/WaterTrackerModal/WaterTrackerModal.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+
+import WaterTrackerModal from "./WaterTrackerModal.svelte";
+
+const meta = {
+  title: "Pages/WaterTrackerModal",
+  component: WaterTrackerModal,
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/svelte/configure/story-layout
+    layout: "fullscreen",
+    docs: {
+      autodocs: false,
+    },
+  },
+} satisfies Meta<WaterTrackerModal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
