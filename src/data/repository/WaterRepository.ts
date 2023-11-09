324a23292e3a2e7363dac989a93aef7a788e3950
daavsnts <daavsantosl@gmail.com>
2023-11-09 08:34:51 -0300

    WaterRepository (feat): add today() functions

--- src/data/repository/WaterRepository.ts
+++ src/data/repository/WaterRepository.ts
@@ -3,10 +3,14 @@ import type { SQLiteDao } from "../local/SQLiteDao";
 
 export interface WaterRepository {
   getTotalWaterIntake: () => Promise<number>;
+  getTodayTotalWaterIntake: () => Promise<number>;
   getWaterIntakeHistory: () => Promise<WaterIntake[]>;
+  getTodayWaterIntakeHistory: () => Promise<WaterIntake[]>;
   getLatestWaterGoal: () => Promise<WaterGoal>;
+  getTodayLatestWaterGoal: () => Promise<WaterGoal>;
   addWaterIntake: (quantity: number) => Promise<boolean>;
   updateWaterGoal: (quantity: number) => Promise<boolean>;
+  updateTodayWaterGoal: (quantity: number) => Promise<boolean>;
 }
 
 export async function createWaterRepository(
@@ -21,12 +25,27 @@ export async function createWaterRepository(
     }, 0);
   }
 
+  async function getTodayTotalWaterIntake(): Promise<number> {
+    const todayWaterIntakeHistory = await dao.getTodayWaterIntakeHistory();
+    return todayWaterIntakeHistory.reduce((acc, currentValue) => {
+      return acc + currentValue.quantity;
+    }, 0);
+  }
+
   async function getWaterIntakeHistory(): Promise<WaterIntake[]> {
     return await dao.getWaterIntakeHistory();
   }
 
+  async function getTodayWaterIntakeHistory(): Promise<WaterIntake[]> {
+    return await dao.getTodayWaterIntakeHistory();
+  }
+
   async function getLatestWaterGoal(): Promise<WaterGoal> {
     return await dao.getLatestWaterGoal();
+  }
+
+  async function getTodayLatestWaterGoal(): Promise<WaterGoal> {
+    return await dao.getTodayLatestWaterGoal();
   }
 
   async function addWaterIntake(quantity: number): Promise<boolean> {
@@ -52,9 +71,13 @@ export async function createWaterRepository(
 
   return {
     getTotalWaterIntake,
+    getTodayTotalWaterIntake,
     getWaterIntakeHistory,
+    getTodayWaterIntakeHistory,
     getLatestWaterGoal,
+    getTodayLatestWaterGoal,
     addWaterIntake,
     updateWaterGoal,
+    updateTodayWaterGoal,
   };
 }
