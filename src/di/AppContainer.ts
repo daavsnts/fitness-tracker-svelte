65b9a5e80845c2d0c4a8d9a2ba0f9029e012b7a5
daavsnts <daavsantosl@gmail.com>
2023-11-09 12:55:06 -0300

    AppContainer (feat): add ExerciseRepository

--- src/di/AppContainer.ts
+++ src/di/AppContainer.ts
@@ -5,23 +5,36 @@ import {
   createWaterRepository,
   type WaterRepository,
 } from "../data/repository/WaterRepository";
+import {
+  createExerciseRepository,
+  type ExerciseRepository,
+} from "../data/repository/ExerciseRepository";
+import { createSQLiteExerciseDao } from "../data/local/SQLiteExerciseDao";
 
 export interface AppContainer {
   getWaterRepository: () => Promise<WaterRepository>;
+  getExerciseRepository: () => Promise<ExerciseRepository>;
 }
 
 function createAppContainer(): AppContainer {
   const dbConnection = createSQLiteDatabase();
   const dao = createSQLiteDao(dbConnection);
   const waterDao = createSQLiteWaterDao(dao);
+  const exerciseDao = createSQLiteExerciseDao(dao, dbConnection);
   const waterRepository = createWaterRepository(waterDao);
+  const exerciseRepository = createExerciseRepository(exerciseDao);
 
   async function getWaterRepository(): Promise<WaterRepository> {
     return await waterRepository;
   }
 
+  async function getExerciseRepository(): Promise<ExerciseRepository> {
+    return await exerciseRepository;
+  }
+
   return {
     getWaterRepository,
+    getExerciseRepository,
   };
 }
 
