98d9a3c015001d546cde6ae9e954a3e0d5078f93
daavsnts <daavsantosl@gmail.com>
2023-11-09 10:58:23 -0300

    RoundedIconButton (feat): add RoundedIconButton component

---
+++ src/components/RoundedIconButton/RoundedIconButton.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let onClickFunction: Function;
+  export let alt: string;
+  export let iconPath: string;
+</script>
+
+<button on:click={() => onClickFunction()}>
+  <img
+    src={iconPath}
+    {alt}
+  />
+</button>
+
+<style lang="scss">
+  button {
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    border: none;
+    filter: drop-shadow(1px 1px 4px rgba(0, 0, 0, 0.3));
+  }
+</style>
